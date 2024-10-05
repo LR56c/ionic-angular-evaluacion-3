07790398b25b9b0c75cabdb41e786850576a006d
Mauricio Herrera Ortega <ronalli56@gmail.com>
2024-10-05 19:56:25 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-angular-evaluacion-3',
+  webDir: 'www'
+};
+
+export default config;
